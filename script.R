eb0f374964ff84146b6fed622c30b9f458f71438
Ondra <zachaondrej@gmail.com>
2017-08-09 23:58:47 +0200

    add ngrams extraction

--- script.R
+++ script.R
@@ -2,16 +2,25 @@ library(tidyverse)
 library(tidytext)
 library(rvest)
 
+## set some locale that works  # https://github.com/juliasilge/tidytext/issues/63
+Sys.setlocale(locale = "Czech_Czech Republic.1250")  # probably not this one
+
 # https://www.w3schools.com/cssref/css_selectors.asp
 
-HTM <- read_html("https://www.novinky.cz/domaci/445854-cesko-podalo-zalobu-na-evropskou-smernici-o-zbranich.html", encoding = "UTF-8")
+HTM <- read_html("https://www.novinky.cz/domaci/445854-cesko-podalo-zalobu-na-evropskou-smernici-o-zbranich.html")
 
+## get text from html
 body <- html_nodes(HTM, "div.articleBody p") %>% 
-  html_text() %>%
-  paste(collapse = " ")
+  html_text()  # %>% paste(collapse = " ) 
 
 perex <- html_nodes(HTM, "div#articleHeaderBig p") %>% 
   html_text()
 
 title <- html_nodes(HTM, "div#articleHeaderBig h1") %>% 
   html_text()
+
+## get ngrams
+bigrams <- body %>% 
+  data_frame(text = .) %>% 
+  unnest_tokens(bigram, text, token = "ngrams", n = 2, collapse = FALSE)
+bigrams
